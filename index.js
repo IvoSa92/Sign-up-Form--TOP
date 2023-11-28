9f5b6ffd0d462d068287970b4fe2f7ef5e6964b7
IvoSa92 <ivo_sauter@web.de>
2023-11-28 19:06:49 +0100

    writing code for event listener and password check for the password inputs

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const passwordInputDiv = document.querySelector(".password-input");
 const passwordInput = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+let errorMessage;
 
-document.addEventListener("DOMContentLoaded", () => {
-  if (passwordInput.value === "") {
-    let errorMessage = document.createElement("p");
-    errorMessage.textContent = "*Passwords do not match";
-    passwordInputDiv.appendChild(errorMessage);
-    errorMessage.classList.add("password-error");
-  }
-});
+document.addEventListener("DOMContentLoaded", checkInput);
+passwordInput.addEventListener("input", checkInput);
+confirmPassword.addEventListener("input", checkInput);
+
+function checkInput() {
+  if (
+    passwordInput.value == "" ||
+    passwordInput.value !== confirmPassword.value
+  )
+    if (!errorMessage) {
+      {
+        errorMessage = document.createElement("p");
+        errorMessage.textContent = "*Passwords do not match";
+        errorMessage.classList.add("password-error");
+        passwordInputDiv.appendChild(errorMessage);
+      }
+    } else if (errorMessage) {
+      errorMessage.remove();
+      errorMessage = null;
+    }
+}
